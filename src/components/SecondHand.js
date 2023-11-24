0c28aaf9d3b1353828216f0b0da260af9f6a1c32
Tim <62808178+timothymaxwellsummers@users.noreply.github.com>
2023-11-24 22:31:10 +0100

    Added first watch face
    Also fixed some bugs

--- src/components/SecondHand.js
+++ src/components/SecondHand.js
@@ -1,21 +1,22 @@
-import React, { useEffect, useState } from 'react'; // Import your second hand image
+import React, { useEffect, useState } from 'react';
 import handStyles from '../styles/hands.module.css';
 
 export default function SecondHand() {
-    const [seconds, setSeconds] = useState(new Date().getSeconds());
+    const [degrees, setDegrees] = useState(() => {
+        const currentSeconds = new Date().getSeconds();
+        return currentSeconds * 6; // Initial degree based on current seconds
+    });
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setSeconds(new Date().getSeconds());
+            setDegrees(prevDegrees => prevDegrees + 6); // Add 6 degrees every second
         }, 1000);
 
         return () => clearInterval(interval);
     }, []);
 
-    const rotationDegree = seconds * 6; // 360 degrees / 60 seconds / replace 100 with 6
-
     return (
-        <div className={handStyles.secondHandContainer} style={{ transform: `rotate(${rotationDegree}deg)` }}>
+        <div className={handStyles.secondHandContainer} style={{ transform: `rotate(${degrees}deg)` }}>
             <img src={"/secondHand.png"} alt="Second Hand" className={handStyles.secondHandImage}/>
         </div>
     );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,19 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from 'next/image';
 import SecondHand from '../components/SecondHand';
-
-const inter = Inter({ subsets: ['latin'] })
+import styles from '../styles/index.module.css'; // Ensure you have this CSS module
 
 export default function Home() {
   return (
-    <main>
+    <main className={styles.mainContainer}>
+      <div className={styles.backgroundImageContainer}>
+        <Image
+          src="/CartierWatchfaceTrans.png" // Replace with your image path
+          alt="Background"
+          layout="fill" // This makes the image fill the container
+          objectFit="cover" // This keeps the aspect ratio and covers the area
+        />
+      </div>
       <SecondHand />
     </main>
-  )
+  );
 }
