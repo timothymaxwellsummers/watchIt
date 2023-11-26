98bfeadb130cde29decd7719d2637a773b0106cd
Tim <62808178+timothymaxwellsummers@users.noreply.github.com>
2023-11-26 22:20:40 +0100

    Added dynamic shadows

--- src/components/MinuteHand.js
+++ src/components/MinuteHand.js
@@ -2,9 +2,14 @@ import React from 'react';
 import handStyles from '../styles/hands.module.css';
 
 export default function MinuteHand({ degrees }) {
+    const shadowX = Math.sin((degrees - 90) * (Math.PI / 180)) * 2.5; // Shadow length and direction on X-axis
+    const shadowY = Math.cos((degrees - 90) * (Math.PI / 180)) * 2.5; // Shadow length and direction on Y-axis
+
     return (
         <div className={handStyles.minuteHandContainer} style={{ transform: `rotate(${degrees}deg)` }}>
-            <img src={"/minuteHand.png"} alt="Minute Hand" className={handStyles.minuteHandImage}/>
+            <img src={"/minuteHand.png"} alt="Minute Hand"
+                style={{ filter: `drop-shadow(${shadowX}px ${shadowY}px 2px #5e5e5e)` }}
+                className={handStyles.minuteHandImage} />
         </div>
     );
 }
--- src/components/SecondHand.js
+++ src/components/SecondHand.js
@@ -2,9 +2,14 @@ import React from 'react';
 import handStyles from '../styles/hands.module.css';
 
 export default function SecondHand({ degrees }) {
+    const shadowX = Math.sin((degrees - 90) * (Math.PI / 180)) * 2.5; // Shadow length and direction on X-axis
+    const shadowY = Math.cos((degrees - 90) * (Math.PI / 180)) * 2.5; // Shadow length and direction on Y-axis
+
     return (
         <div className={handStyles.secondHandContainer} style={{ transform: `rotate(${degrees}deg)` }}>
-            <img src={"/secondHand.png"} alt="Second Hand" className={handStyles.secondHandImage}/>
+            <img src={"/secondHand.png"} alt="Second Hand" 
+            style={{ filter: `drop-shadow(${shadowX}px ${shadowY}px 2px #5e5e5e)` }}  
+            className={handStyles.secondHandImage}/>
         </div>
     );
 }
