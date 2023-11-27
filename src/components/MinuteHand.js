37a9200b27a821786757987d93fcf499fa506a20
Tim <62808178+timothymaxwellsummers@users.noreply.github.com>
2023-11-27 22:37:41 +0100

    Fixed positioning for the hands

--- src/components/MinuteHand.js
+++ src/components/MinuteHand.js
@@ -5,11 +5,15 @@ export default function MinuteHand({ degrees }) {
     const shadowX = Math.sin((degrees - 90) * (Math.PI / 180)) * 2.5; // Shadow length and direction on X-axis
     const shadowY = Math.cos((degrees - 90) * (Math.PI / 180)) * 2.5; // Shadow length and direction on Y-axis
 
+    // Style for rotating the minute hand
+    const rotationStyle = {
+        transform: `translateY(-94.25%) translateX(-50%) rotate(${degrees}deg)`,
+        filter: `drop-shadow(${shadowX}px ${shadowY}px 2px #5e5e5e)`
+    };
+
     return (
-        <div className={handStyles.minuteHandContainer} style={{ transform: `rotate(${degrees}deg)` }}>
-            <img src={"/minuteHand.png"} alt="Minute Hand"
-                style={{ filter: `drop-shadow(${shadowX}px ${shadowY}px 2px #5e5e5e)` }}
-                className={handStyles.minuteHandImage} />
+        <div className={handStyles.minuteHandContainer} style={rotationStyle}>
+            <img src={"/minuteHand.png"} alt="Minute Hand" className={handStyles.minuteHandImage} />
         </div>
     );
 }
--- src/components/SecondHand.js
+++ src/components/SecondHand.js
@@ -5,11 +5,15 @@ export default function SecondHand({ degrees }) {
     const shadowX = Math.sin((degrees - 90) * (Math.PI / 180)) * 2.5; // Shadow length and direction on X-axis
     const shadowY = Math.cos((degrees - 90) * (Math.PI / 180)) * 2.5; // Shadow length and direction on Y-axis
 
+    // Style for rotating the minute hand
+    const rotationStyle = {
+        transform: `translateY(-80.8%) translateX(-50%) rotate(${degrees}deg)`,
+        filter: `drop-shadow(${shadowX}px ${shadowY}px 2px #5e5e5e)`
+    };
+
     return (
-        <div className={handStyles.secondHandContainer} style={{ transform: `rotate(${degrees}deg)` }}>
-            <img src={"/secondHand.png"} alt="Second Hand" 
-            style={{ filter: `drop-shadow(${shadowX}px ${shadowY}px 2px #5e5e5e)` }}  
-            className={handStyles.secondHandImage}/>
+        <div className={handStyles.secondHandContainer} style={rotationStyle}>
+            <img src={"/secondHand.png"} alt="Second Hand" className={handStyles.secondHandImage}/>
         </div>
     );
 }
