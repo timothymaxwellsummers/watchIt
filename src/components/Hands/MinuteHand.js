51023fe8ff7cc5e7b0191ee1d9bfde3d1e15e988
Tim <62808178+timothymaxwellsummers@users.noreply.github.com>
2023-12-29 17:47:30 +0100

    Updated minuteHand
    Correct layout for Seamaster

--- src/components/Hands/MinuteHand.js
+++ src/components/Hands/MinuteHand.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import handStyles from '../../styles/hands.module.css';
+import minuteStyles from '../../styles/minuteHand.module.css';
 
 export default function MinuteHand({ degrees, name }) {
     const containerRef = useRef(null);
@@ -14,6 +15,7 @@ export default function MinuteHand({ degrees, name }) {
 
             // Instantly jump to the desired degrees
             containerRef.current.style.transform = `translateY(-94.25%) translateX(-50%) rotate(${degrees}deg)`;
+            containerRef.current.style.transform = name === 'Cartier' ? `translateY(-94.25%) translateX(-50%) rotate(${degrees}deg)` : `translateY(-92%) translateX(-50%) rotate(${degrees}deg)`;
 
             // Force a reflow
             void containerRef.current.offsetWidth;
@@ -27,18 +29,27 @@ export default function MinuteHand({ degrees, name }) {
 
 
     // Style for rotating the minute hand
-    const rotationStyle = {
+    const rotationStyleCartier = {
         transform: `translateY(-94.25%) translateX(-50%) rotate(${degrees}deg)`,
         filter: `drop-shadow(${Math.sin((degrees - 90) * (Math.PI / 180)) * 2.5}px ${Math.cos((degrees - 90) * (Math.PI / 180)) * 2.5}px 2px #5e5e5e)`
     };
 
+    const rotationStyleSeamaster = {
+        transform: `translateY(-92%) translateX(-50%) rotate(${degrees}deg)`,
+        filter: `drop-shadow(${Math.sin((degrees - 90) * (Math.PI / 180)) * 2.5}px ${Math.cos((degrees - 90) * (Math.PI / 180)) * 2.5}px 2px #5e5e5e)`
+    };
+
     return (
-        <div ref={containerRef} className={handStyles.minuteHandContainer} style={rotationStyle}>
-            {name === "Cartier" ? (
+        <>
+        { name === "Cartier" ?
+            <div ref={containerRef} className={minuteStyles.cartierMinute} style={rotationStyleCartier}>
                 <img src={"/minuteHand.png"} alt="Minute Hand" className={handStyles.minuteHandImage} />
-            ) : (
+            </div>
+            :
+            <div ref={containerRef} className={minuteStyles.seamasterMinute} style={rotationStyleSeamaster}>
                 <img src={"/SeamasterMinuteHand.png"} alt="Minute Hand" className={handStyles.minuteHandImage} />
-            )}
-        </div>
+            </div>
+        }
+        </>
     );
 }
