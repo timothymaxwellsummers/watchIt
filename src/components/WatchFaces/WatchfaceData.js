53e2e95f557d3cc9834af199966875530361ae2d
Tim <62808178+timothymaxwellsummers@users.noreply.github.com>
2023-12-29 22:18:25 +0100

    Updated design of dots
    adapted to seamaster watchface

--- src/components/WatchFaces/WatchfaceData.js
+++ src/components/WatchFaces/WatchfaceData.js
@@ -3,6 +3,8 @@ export const watchFaces = [
       name: "Cartier",
       imageUrl: "/CartierWatchface.png",
       backgroundColor: "#ffffff", // Example background color
+      activeDotColor: "#000000", // Example inactive dot color
+      inactiveDotColor: "#050505", // Example inactive dot color
       secondHand: { /* properties specific to the second hand */ },
       minuteHand: { /* properties specific to the minute hand */ },
       hourHand: { /* properties specific to the hour hand */ }
@@ -11,6 +13,8 @@ export const watchFaces = [
       name: "Seamaster",
       imageUrl: "/SeamasterWatchface.png",
       backgroundColor: "#003154", // Example background color
+      activeDotColor: "#fff", // Example inactive dot color
+      inactiveDotColor: "#78afef", // Example inactive dot color
       secondHand: { /* properties specific to the second hand */ },
       minuteHand: { /* properties specific to the minute hand */ },
       hourHand: { /* properties specific to the hour hand */ }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import Slider from "react-slick";
 export default function Home() {
   const [currentTime, setCurrentTime] = useState(new Date());
   const { stopWatchMode, elapsedTime } = useStopWatch();
+  const [activeWatchFace, setActiveWatchFace] = useState(watchFaces[0]); // default to the first watch face
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -58,8 +59,25 @@ export default function Home() {
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
-    adaptiveHeight: true
+    adaptiveHeight: true,
+    //@ts-ignore
+    beforeChange: (current, next) => setActiveWatchFace(watchFaces[next]),
   };
+
+  const dotStyles = {
+    // Style for all dots
+    '.slick-dots li button:before': {
+      color: activeWatchFace.inactiveDotColor, // Use a color from your watchFace data
+    },
+    // Style for the active dot
+    '.slick-dots li.slick-active button:before': {
+      color: activeWatchFace.activeDotColor, // Use a color from your watchFace data
+    },
+  };
+
+  const dotStylesString = Object.entries(dotStyles)
+  .map(([selector, style]) => `${selector} { ${Object.entries(style).map(([prop, value]) => `${prop}: ${value}`).join('; ')} }`)
+  .join(' ');
 
   // Keep increasing degrees beyond 360
   const secondsDegrees = stopWatchMode ? (elapsedTime / 1000) * 6 : (currentTime.getMinutes() * 60 + currentTime.getSeconds()) * 6;
@@ -68,6 +86,7 @@ export default function Home() {
 
   return (
     <div style={{ position: 'relative', }}>
+          <style>{dotStylesString}</style> {/* Include the dynamic styles */}
       <Slider {...settings}>
       {watchFaces.map(watchFace => (
           <WatchFace
